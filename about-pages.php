cbf702dfcd291998a060cf5adbb142aad26c02a6
Mona Lisa <lisa.kaiser21@gmail.com>
2017-10-01 15:47:59 -0700

    made changes to about-pages

--- about-pages.php
+++ about-pages.php
@@ -20,7 +20,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="about-us hero-content">
+	<div id="primary" class="about-page hero-content">
 		<div class="main-content" role="main">
 		  <?php while ( have_posts() ) : the_post(); 
 		  <?php the_content(); ?>
@@ -28,21 +28,27 @@ get_header(); ?>
 		</div><!-- .main-content -->
 	 </div><!-- #primary -->
 			
-	 <article class="service-custom">
+	 <section class="service-custom">
 		<aside class="service-custom-sidebar">
 		   <h2><?php the_title(); ?></a></h2>
 	    <h5><?php echo $services; ?></h5>
+		
+		<div class="services">
+          <h2>Our Services</h2>
+            <p>We take pride in our clients and the content we create for them.<b>Here's a brief overview of our offered services.</p>
+        </div>
 		 
-	  <?php the_excerpt(); ?>
-	
+	 	
 		</aside>
 		
-	   <div class="case-study-images">
+	   <div class="service-custom-images">
 		  <a href="<?php the_permalink(); ?>">
 		  <?php if ($image_1) {
 		  echo wp_get_attachment_image($image_1, $size); } ?>
 		</div>
-	  </article>
+		
+		<a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
+	  </section>
 			
 	  <?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
