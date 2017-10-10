ee0f44aaed7a27d0e69525cd8e3396de255c679f
Mona Lisa <lisa.kaiser21@gmail.com>
2017-10-09 21:58:36 -0700

    made changes to the about page

--- page-about.php
+++ page-about.php
@@ -8,55 +8,52 @@
  */
 
 
-get_header(); ?>
-
-	<div id="primary" class="about-page hero-content">
-		<div class="main-content" role="main">
-		 <?php while ( have_posts() ) : the_post(); ?>
-
-				<?php the_content(); ?>
-				
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	   </div><!-- #primary -->
-		
-	 
-		<section class="our-services">
-		  <div class="site-content">
-            <h2>Our Services</h2>
-		    <p>We take pride in our clients and the content we create for them.<br>
-		     Here's a brief overview of our offered services.</p>
-          </div>
-	 
-		<div class="about-services">
-	      <?php query_posts('posts_per_page=4&post_type=service_customs'); ?>
+	get_header(); ?>
 	
-	<!-- start the loop -->
-		<?php while ( have_posts() ) : the_post();
-			$services = get_field('services');
-		    $image_1 = get_field('image_1');
-		    $image_2 = get_field('image_2');
-            $image_3 = get_field('image_3');
-			$image_4 = get_field('image_4');
-		    $size = "medium";
-		?>
-		 
-		<article class="service-custom-images">
-		 <figure>
-		   <?php echo wp_get_attachment_image($image_1, $size); ?>
-		 </figure>
-		
+	<div id="primary" class="page-about hero-content">
+	  <div class="site-content" role="main">
+	     <?php while ( have_posts() ) : the_post(); ?> 
+		   <?php the_content(); ?>
 		   <?php endwhile; //end of the loop. ?>
-		    <?php wp_reset_query(); // resets the altered query back to the original ?>
-		  </div>
-		  </article>
-		  
-		<div class="service-button">
-		  <h3><a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a></h3>
-		 </div>
-	</section>
-	 	 
-	  
+		</div><!-- .site-content -->
+	</div><!-- .about-page -->
+	
+	<section class="about-services">
+	  <div class="about-content">
+	    <h4>Our Services</h4>
+		<p>We take pride in our clients and the content we create for them.<br>
+		Here's a brief overview of our offered services.</p>
 		
+	
+    <article class="about-page-services">		
+	
+	<?php query_posts('posts_per_page=4&post_type=service_customs'); ?>
+	 <?php while (have_posts() ) : the_post();
+		$services = get_field('services');
+		$image_1 = get_field('image_1');
+		$size = "medium";
+	?>
+	
+	<aside class="about-page-services">	
+	 		
+	  <h3><?php echo $services; ?></h3>
+	  
+	<div class="individual-services">
+	  <figure>
+		<?php echo wp_get_attachment_image($image_1, $size); ?>
+	  </figure>
+
+	  	 
+	<div class="services-content">
+	   <?php the_content(); ?> 
+	  </div>
+	 </aside>
+	</article>
+	</section>
+    			 
+	<?php endwhile; //end of the loop. ?>
+	  <?php wp_reset_query(); // resets the altered query back to the original ?>
+	   </div>
+	   </div>
 
 <?php get_footer(); ?>
--- single-service_customs.php
+++ single-service_customs.php
@@ -11,20 +11,21 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
-		  <?php while ( have_posts() ) : the_post(); 
-			$services = get_field('services');
-			$image_1 = get_field('image_1');
-			$image_2 = get_field('image_2');
-			$image_3 = get_field('image_3');
-			$image_4 = get_field('image_4');
-		?>
-			
-	 <article class="service-custom">
-		<aside class="service-custom-sidebar">
-		  <h2><?php the_title(); ?></a></h2>
-		   <h5><?php echo $services; ?></h5>
-		</aside>
 		
+	<?php while ( have_posts() ) : the_post(); 
+		$services = get_field('services');
+		$link = get_field('site_link');
+		$image_1 = get_field('image_1');
+		$image_2 = get_field('image_2');
+		$image_3 = get_field('image_3');
+		$image_4 = get_field('image_4');
+	?>
+	
+		<article class="service-customs">
+		<aside class="service-customs-sidebar">
+		
+		<h5><?php echo $services; ?></h5>
+			
 	    <div class="service-custom-images">
 		  <?php echo wp_get_attachment_image($image_1, $size); ?>
 	      <?php echo wp_get_attachment_image($image_2, $size); ?>
@@ -32,6 +33,9 @@ get_header(); ?>
 		  <?php echo wp_get_attachment_image($image_4, $size); ?>
 		</div>
 	  </article>
+	  
+	  <?php the_content(); ?>
+	</aside>
 			
 	  <?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
