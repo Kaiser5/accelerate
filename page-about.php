1c37422ac75afffc21094bcfa7fd48a1a366f7ff
Mona Lisa <lisa.kaiser21@gmail.com>
2017-10-14 18:19:17 -0700

    Additional changes to the about page

--- page-about.php
+++ page-about.php
@@ -20,40 +20,43 @@
 	
 	<section class="about-services">
 	  <div class="about-content">
-	    <h4>Our Services</h4>
-		<p>We take pride in our clients and the content we create for them.<br>
+	    <h2>Our Services</h2>
+	  <p>We take pride in our clients and the content we create for them.<br>
 		Here's a brief overview of our offered services.</p>
 		
 	
-    <article class="about-page-services">		
-	
+	 <article class="about-page-services">	
+      <aside class="services">	
+		   	
 	<?php query_posts('posts_per_page=4&post_type=service_customs'); ?>
 	 <?php while (have_posts() ) : the_post();
 		$services = get_field('services');
 		$image_1 = get_field('image_1');
 		$size = "medium";
 	?>
+ 	
+	<div class="individual-services">	
+	  	
+	<figure>
+	  <?php echo wp_get_attachment_image($image_1, $size); ?>
+	</figure>
 	
-	<aside class="about-page-services">	
-	 		
-	  <h3><?php echo $services; ?></h3>
-	  
-	<div class="individual-services">
-	  <figure>
-		<?php echo wp_get_attachment_image($image_1, $size); ?>
-	  </figure>
-
-	  	 
+		  	 
 	<div class="services-content">
-	   <?php the_content(); ?> 
-	  </div>
-	 </aside>
-	</article>
-	</section>
-    			 
+	   <h2><?php the_title(); ?></h2><?php the_content(); ?>
+	</div>
+	 
+	     			 
 	<?php endwhile; //end of the loop. ?>
 	  <?php wp_reset_query(); // resets the altered query back to the original ?>
-	   </div>
-	   </div>
+		
+		</div>
+	   </aside>
+	  </article>
+	 </div>
+	</section>
+
+
+	
 
 <?php get_footer(); ?>
